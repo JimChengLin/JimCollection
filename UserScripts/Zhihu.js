05570d4c1d3b7251d36a214091548ae89f035ea4
JimLin <jimzuolin@gmail.com>
2016-07-05 00:08:23 +0800

    逻辑优化

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -1,74 +1,61 @@
-'use strict';
-
-if (location.pathname.startsWith('/story')) {
-    $(() => {
-        $('.question:last-child').not(':contains("查看知乎原文")').remove();
+$('<style>@media screen and (max-width:1120px){.zu-top{display:none;}}</style>').appendTo('html');
+$(window)
+    .on('copy', () => {
+        GM_setClipboard(getSelection().toString(), 'text');
+    })
+    .on('click', (event) => {
+        var link = event.target;
+        if (link.parentElement && link.parentElement.tagName === 'A') {
+            link = link.parentElement;
+        }
+        var sign = 'link.zhihu.com/?target=';
+        if (link.href && link.href.includes(sign)) {
+            link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
+        }
     });
-} else {
-    $('<style>@media screen and (max-width: 1120px){.zu-top{display:none;}}</style>').appendTo('html');
-    $(window)
-        .on('copy', () => {
-            GM_setClipboard(getSelection().toString(), 'text');
-        })
-        .on('click', (event) => {
-            var link = event.target;
-            if (link.parentElement && link.parentElement.tagName === 'A') {
-                link = link.parentElement;
-            }
-            const sign = 'link.zhihu.com/?target=';
-            if (link.href && link.href.includes(sign)) {
-                link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
-            }
-        });
 
-    if (location.pathname.endsWith('/topic')) {
-        $(ZhihuFilter);
-    }
-}
+if (location.pathname.endsWith('/topic')) {
+    $(()=> {
+        var DAY_NOW = Math.round(Date.now() / 1000 / 60 / 60 / 24);
 
-function ZhihuFilter() {
-    const DAY_NOW = Math.round(Date.now() / 1000 / 60 / 60 / 24);
-
-    var dayDiff = {
-        init: () => {
-            this.record = {};
-            for (let url of GM_listValues()) {
-                let diff = DAY_NOW - GM_getValue(url);
-                diff > 21 ? GM_deleteValue(url) : this.record[url] = diff;
-            }
-        },
-
-        search: (url) => {
-            if (url in this.record) {
-                return this.record[url];
-            } else {
-                GM_setValue(url, DAY_NOW);
-                return this.record[url] = 0;
-            }
-        }
-    };
-    dayDiff.init();
-
-    var change;
-    var observer = new MutationObserver(() => change = true);
-    observer.observe(document.body, {childList: true, subtree: true});
-
-    setInterval(() => {
-        if (change) {
-            $('.question_link').map((i, element)=> {
-                element = $(element);
-                if (element.hasClass('_pass')) {
-                    return;
+        var dayDiff = {
+            init: () => {
+                this.record = {};
+                for (var url of GM_listValues()) {
+                    var diff = DAY_NOW - GM_getValue(url);
+                    diff > 21 ? GM_deleteValue(url) : this.record[url] = diff;
+                }
+            },
+            search: (url) => {
+                if (url in this.record) {
+                    return this.record[url];
                 } else {
-                    element.addClass('_pass');
+                    GM_setValue(url, DAY_NOW);
+                    return this.record[url] = 0;
                 }
+            }
+        };
+        dayDiff.init();
 
-                let item = element.closest('.feed-item');
-                if (dayDiff.search(element.attr('href')) > 7 || dayDiff.search(item.find('link').attr('href')) > 1) {
-                    item.hide();
-                }
-            });
-            change = false;
-        }
-    }, 1000);
+        var change;
+        var observer = new MutationObserver(() => change = true);
+        observer.observe(document.querySelector('div.zu-main-content > div'), {childList: true, subtree: true});
+
+        setInterval(() => {
+            if (change) {
+                $('.question_link:not(._pass)').map((i, element) => {
+                    element = $(element);
+                    if (element.offset().top < document.body.scrollTop + innerHeight) {
+                        element.addClass('_pass');
+                        var item = element.closest('.feed-item');
+                        if (dayDiff.search(element.attr('href')) > 7 ||
+                            dayDiff.search(item.find('link').attr('href')) > 1) {
+                            item.remove();
+                        }
+                    }
+                });
+                change = false;
+            }
+        }, 1000);
+    });
 }
