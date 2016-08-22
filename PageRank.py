b83b825300c84a8cb9a92e76795f46ef67ea3e0e
yuanjinlin <linyuanjin@yscredit.com>
2016-08-22 15:14:49 +0800

    稀疏矩阵的PR完毕

--- PageRank.py
+++ PageRank.py
@@ -14,7 +14,7 @@ DAMPING_FACTOR = 0.15
 def calc_without_random() -> list:
     result = []
     for row in M:
-        result.append(a * b for a, b in zip(row, V))
+        result.append(sum(a * b for a, b in zip(row, V)))
     return result
 
 
@@ -29,5 +29,34 @@ def calc_with_random() -> list:
     return result
 
 
+M_DICT = {0: {2: 1, 3: 1 / 2},
+          1: {0: 1 / 3},
+          2: {0: 1 / 3, 1: 1 / 2, 3: 1 / 2},
+          3: {0: 1 / 3, 1: 1 / 2}}
+
+
+def calc_dict_without_random():
+    result = []
+    for i_col in range(4):
+        total = 0
+        for i_row in range(4):
+            total += M_DICT[i_col].get(i_row, 0) * V[i_row]
+        result.append(total)
+    return result
+
+
+def calc_dict_with_random():
+    result = []
+    for i_col in range(4):
+        total = 0
+        for i_row in range(4):
+            total += trans(M_DICT[i_col].get(i_row, 0)) * V[i_row]
+        result.append(total)
+    return result
+
+
 if __name__ == '__main__':
+    print(calc_without_random())
+    print(calc_dict_without_random())
     print(calc_with_random())
+    print(calc_dict_with_random())
