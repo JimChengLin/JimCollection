7a4cd8c886067d462b4c8a523b067a756197445a
Jim <jimzuolin@gmail.com>
2016-08-22 21:32:41 +0800

    条件判断

--- UserScripts/Zhihu.js
+++ UserScripts/Zhihu.js
@@ -12,7 +12,7 @@ switch (location.host) {
 function zhihuDaily() {
     $(() => {
         if (location.pathname.startsWith('/story')) {
-            const target = $('.question:last-child').not(':contains("查看知乎原文")');
+            let target = $('.question:last-child').not(':contains("查看知乎原文")');
             if (target.length && target.text().length < 100) {
                 target.remove();
             }
@@ -31,7 +31,7 @@ function zhihu() {
             if (link.parentElement && link.parentElement.tagName === 'A') {
                 link = link.parentElement;
             }
-            const sign = 'link.zhihu.com/?target=';
+            let sign = 'link.zhihu.com/?target=';
             if (link.href && link.href.includes(sign)) {
                 link.href = decodeURIComponent(link.href.substr(link.href.indexOf(sign) + sign.length));
             }
@@ -44,8 +44,8 @@ function zhihu() {
         });
 
         $(()=> {
-            const DAY_NOW = () => Date.now() / 1000 / 60 / 60 / 24;
-            const DayDiff = {
+            let DAY_NOW = () => Date.now() / 1000 / 60 / 60 / 24;
+            let DayDiff = {
                 record: {},
                 load: () => {
                     for (let url of GM_listValues()) {
@@ -65,21 +65,21 @@ function zhihu() {
             DayDiff.load();
 
             let change = true;
-            const observer = new MutationObserver(() => change = true);
+            let observer = new MutationObserver(() => change = true);
             observer.observe(document.querySelector('div.zu-main-content'), {childList: true, subtree: true});
 
             setInterval(() => {
                 if (change) {
                     $('.feed-item > link').map((i, elem) => {
                         elem = $(elem);
-                        if (DayDiff.search(elem.attr('href')) > 0.6) {
+                        if (DayDiff.search(elem.attr('href')) > 0.7) {
                             elem.closest('.feed-item').fadeOut();
                         }
                     });
                     $('.goog-scrollfloater-floating').remove();
                     change = false;
                 }
-            }, 1000);
+            }, 700);
         });
     }
 }
