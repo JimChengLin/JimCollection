e1428858a882b613c3b24066d063244b887c3b87
yuanjinlin <linyuanjin@yscredit.com>
2016-08-22 19:56:47 +0800

    progress

--- PageRank.py
+++ PageRank.py
@@ -12,20 +12,35 @@ name_db = AsyncDB('name.db')
 data_db = AsyncDB('data.db')
 
 
-# 将Vertex转化为行列式写入硬盘, 为了读取速度, 下标先列再行
+# 为了读取速度, 下标先列再行
 class Vertex:
     counter = 0
 
     def __init__(self, name: str):
         self.name = name
-        self.num = None
+        self.subscript = None
 
-    async def init_num(self):
-        self.num = 0
+    async def async_init(self):
+        # 若已存入硬盘, 无须分配下标
+        subscript = await name_db[self.name]
+        if subscript is None:
+            # 分配到下标并写回
+            subscript = Vertex.counter
+            name_db[self.name] = self.subscript
+            Vertex.counter += 1
+        self.subscript = subscript
 
     def set_weight(self, weight: float):
-        # -1 => 列标 self.num => 行标
-        data_db[(-1, self.num)] = weight
+        assert self.subscript is not None
+        # (-1: 列标, self.subscript: 行标)
+        data_db[(-1, self.subscript)] = weight
 
-    def __repr__(self):
-        return 'name: {}, no.{}'.format(self.name, self.num)
+    def connect_to(self, vertex_subscript_l):
+        # 节点必须一次性连接完毕, 参数可能是Vertex or subscript
+        for v_s in vertex_subscript_l:
+            if isinstance(v_s, Vertex):
+                pass
+            elif isinstance(v_s, int):
+                pass
+            else:
+                raise Exception
