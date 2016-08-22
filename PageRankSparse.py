9a40688593f2841db67699d71d797a70a096d7fa
yuanjinlin <linyuanjin@yscredit.com>
2016-08-22 16:19:31 +0800

    加入权重

--- PageRankSparse.py
+++ PageRankSparse.py
@@ -55,8 +55,28 @@ def calc_dict_with_random():
     return result
 
 
+# 以下全部默认添加阻尼系数
+# -1表示权重
+M_DICT_WEIGHT = {0: {2: 1, 3: 1 / 2, -1: 1},
+                 1: {0: 1 / 3, -1: 2},
+                 2: {0: 1 / 3, 1: 1 / 2, 3: 1 / 2, -1: 3},
+                 3: {0: 1 / 3, 1: 1 / 2, -1: 4}}
+
+
+def calc_dict_with_weight():
+    result = []
+    for i_col in range(4):
+        total = 0
+        for i_row in range(4):
+            total += trans(M_DICT[i_col].get(i_row, 0)) * V[i_row]
+        total += M_DICT_WEIGHT[i_col][-1] * (1 / 4)
+        result.append(total)
+    return result
+
+
 if __name__ == '__main__':
     print(calc_without_random())
     print(calc_dict_without_random())
     print(calc_with_random())
     print(calc_dict_with_random())
+    print(calc_dict_with_weight())
