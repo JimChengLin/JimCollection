55e91283df3b4ea7fefc75777bb29d2e2a46d694
Jim <jimzuolin@gmail.com>
2016-11-15 22:44:43 +0800

    炸开后缀完成

--- SuffixTree_.py
+++ SuffixTree_.py
@@ -6,7 +6,7 @@ class Node:
         self.sub = {}
 
         self.op = self.ed = None
-        self.ptr_to = None
+        self.link_to = None
 
     @property
     def is_root(self):
@@ -14,11 +14,11 @@ class Node:
 
     @property
     def is_leaf(self):
-        return not self.is_root and self.ptr_to is None
+        return not self.is_root and self.link_to is None
 
     @property
-    def is_inter(self):
-        return not self.is_root and self.ptr_to is not None
+    def is_inner(self):
+        return not self.is_root and self.link_to is not None
 
     def __repr__(self):
         if self.is_root:
@@ -66,9 +66,7 @@ class SuffixTree:
 
         self.remainder += 1
 
-        # 1. ac_node 是 root
-        if self.ac_node.is_root and self.ac_offset == 0:
-
+        def case_1():
             # 1.1. 无法坍缩, 建立新的叶节点
             if char not in self.ac_node.sub:
                 leaf_node = Node()
@@ -83,6 +81,10 @@ class SuffixTree:
                 self.ac_direction = collapse_node.op
                 self.ac_offset += 1
 
+        # 1. ac_node 是 root 的初始状态
+        if self.ac_node.is_root and self.ac_offset == 0:
+            case_1()
+
         # 2. 已经坍缩
         else:
             collapse_node = self.ac_node.sub[g_target[self.ac_direction]]
@@ -91,8 +93,8 @@ class SuffixTree:
             if char == g_target[collapse_node.op + self.ac_offset]:
                 self.ac_offset += 1
 
-                # 2.1.1.如果是 inner_node, 坍缩是否达到极限? 是
-                if collapse_node.is_inter \
+                # 2.1.1. 如果是 inner_node, 坍缩是否达到极限? 是
+                if collapse_node.is_inner \
                         and collapse_node.op + self.ac_offset == collapse_node.ed:
                     # todo: implement
                     pass
@@ -101,11 +103,38 @@ class SuffixTree:
             else:
                 while self.remainder > 0:
 
-                    # 2.2.1. 没有 suffix link 用于状态转移
-                    if collapse_node.ptr_to is None:
-                        # todo: implement
-                        pass
+                    # 2.2.1. 没有 suffix link 用于状态转移(坍缩点直连 root)
+                    if collapse_node.link_to is None:
+                        # 原坍缩点成为 inner_node
+                        collapse_node.ed = collapse_node.op + self.ac_offset
 
+                        # 新节点继承 :ed
+                        inherit_node = Node()
+                        inherit_node.op = collapse_node.ed
+                        inherit_node.ed = ':ed'
+                        # 回连
+                        collapse_node.sub[g_target[inherit_node.op]] = inherit_node
+
+                        # 新节点记录 char
+                        leaf_node = Node()
+                        leaf_node.op = self.cursor
+                        leaf_node.ed = ':ed'
+                        collapse_node.sub[g_target[leaf_node.op]] = leaf_node
+                        self.remainder -= 1
+
+                        # 状态转移, 继续爆炸
+                        self.ac_offset -= 1
+                        self.ac_direction += 1
+
+                        if self.ac_offset > 0:
+                            next_collapse_node = self.ac_node.sub[g_target[self.ac_direction]]
+                            collapse_node.link_to = next_collapse_node
+                            collapse_node = next_collapse_node
+
+                        # 累积后缀已炸完
+                        else:
+                            # 回到状态 1.
+                            case_1()
         self.cursor += 1
 
 
@@ -116,4 +145,5 @@ if __name__ == '__main__':
     t.insert('z')
     t.insert('x')
     t.insert('y')
+    t.insert('a')
     t.repr()
