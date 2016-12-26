7ab4c5da016aa93b853d6489f08a8c3297a82f21
Jim <jimzuolin@gmail.com>
2016-12-26 23:36:38 +0800

    修订

--- CritBitTree.py
+++ CritBitTree.py
@@ -2,15 +2,15 @@ class CBInternal:
     def __init__(self):
         self.diff_at = None
         self.mask = None
-        self.crit_is_0 = None
-        self.crit_is_1 = None
+        self.crit_0 = None
+        self.crit_1 = None
 
     def print(self, lv=0):
         intent = '  '
         print(intent * lv, 'diff_at: {}, mask: {}'.format(self.diff_at, bin(self.mask)))
 
         lv += 1
-        for sub in (self.crit_is_0, self.crit_is_1):
+        for sub in (self.crit_0, self.crit_1):
             if isinstance(sub, CBInternal):
                 sub.print(lv)
             else:
@@ -27,7 +27,7 @@ class CBTree:
         else:
             grand, pa, old_bytes = self.find_best_match(new_bytes)
             # 查找哪个字节不同
-            # 我在这里认为各个字符串一定会以 $ 结尾, 所以不考虑子串判断的问题
+            # 我在这里认为各个字符串一定会以 $ 结尾, 所以不考虑子串的问题
             for i in range(min(len(new_bytes), len(old_bytes))):
                 if new_bytes[i] != old_bytes[i]:
                     diff_at = i
@@ -54,17 +54,17 @@ class CBTree:
             node.diff_at = diff_at
             node.mask = mask
             if direct == 0:
-                node.crit_is_0 = new_bytes
+                node.crit_0 = new_bytes
             else:
-                node.crit_is_1 = new_bytes
+                node.crit_1 = new_bytes
 
             # 插入节点:
             # 因为分歧点是按顺序发现的, 无论这次的分歧点在于路径上的前面还是后面
             # 都可以直接插入而保持性质不变
-            # 即, 分歧顺序按照 1st 2nd 3rd 4th... 排列
+            # 即, 分歧顺序按照 1st 2nd 3rd 4th 排列
             # 比如, 当前在 3rd 处分歧. 那么, 剩余所有已存在的串 3rd 必然没有分歧
             parent = None
-            insert_point = self.root  # 置入 new_node 的位置
+            insert_point = self.root  # 用 new_node 替换的节点
             while True:
 
                 if isinstance(insert_point, bytes):
@@ -73,29 +73,30 @@ class CBTree:
                     if insert_point.diff_at > diff_at:
                         break
                     elif insert_point.diff_at == diff_at and insert_point.mask > node.mask:
-                        # 同一 byte, 分歧 bit 越靠后, 数值越小
+                        # 同 byte 位置, 分歧 bit 越靠后, 数值越小
                         break
 
                 check_byte = new_bytes[insert_point.diff_at] if len(new_bytes) > insert_point.diff_at else 0
-                parent = insert_point
                 direct = (1 + (insert_point.mask | check_byte)) >> 8
+
+                parent = insert_point
                 if direct == 0:
-                    insert_point = insert_point.crit_is_0
+                    insert_point = insert_point.crit_0
                 else:
-                    insert_point = insert_point.crit_is_1
+                    insert_point = insert_point.crit_1
 
             if parent is None:
                 self.root = node
             else:
                 if direct == 0:
-                    parent.crit_is_0 = node
+                    parent.crit_0 = node
                 else:
-                    parent.crit_is_1 = node
+                    parent.crit_1 = node
 
-            if node.crit_is_0 is None:
-                node.crit_is_0 = insert_point
+            if node.crit_0 is None:
+                node.crit_0 = insert_point
             else:
-                node.crit_is_1 = insert_point
+                node.crit_1 = insert_point
 
     def find_best_match(self, src: bytes):
         q = [None] * 3
@@ -107,9 +108,9 @@ class CBTree:
             direct = (1 + (cursor.mask | check_byte)) >> 8
 
             if direct == 0:
-                cursor = cursor.crit_is_0
+                cursor = cursor.crit_0
             else:
-                cursor = cursor.crit_is_1
+                cursor = cursor.crit_1
 
             del q[0]
             q.append(cursor)
