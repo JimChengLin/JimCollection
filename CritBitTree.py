f0c49cda822c434f00eb2db0b6d432be72741b45
Jim <jimzuolin@gmail.com>
2016-12-26 23:26:02 +0800

    插入写完, 接下来验证想法

--- CritBitTree.py
+++ CritBitTree.py
@@ -42,7 +42,7 @@ class CBTree:
             diff_byte |= diff_byte >> 2
             diff_byte |= diff_byte >> 4
 
-            # 0b0111_1111 => 0b1011_1111
+            # 结果: 0b0111_1111 => 0b1011_1111
             # 过程:
             # ~(diff_byte >> 1) = 0b1100_0000
             # diff_byte & 0b1100_0000 = 0b0111_1111 & 0b1100_0000 = 0b0100_0000
@@ -50,16 +50,52 @@ class CBTree:
 
             direct = (1 + (mask | new_bytes[diff_at])) >> 8
             # 生成一个新节点
-            new_node = CBInternal()
-            new_node.diff_at = diff_at
-            new_node.mask = mask
+            node = CBInternal()
+            node.diff_at = diff_at
+            node.mask = mask
             if direct == 0:
-                new_node.crit_is_0 = new_bytes
+                node.crit_is_0 = new_bytes
             else:
-                new_node.crit_is_1 = new_bytes
+                node.crit_is_1 = new_bytes
 
-            # 插入节点
+            # 插入节点:
+            # 因为分歧点是按顺序发现的, 无论这次的分歧点在于路径上的前面还是后面
+            # 都可以直接插入而保持性质不变
+            # 即, 分歧顺序按照 1st 2nd 3rd 4th... 排列
+            # 比如, 当前在 3rd 处分歧. 那么, 剩余所有已存在的串 3rd 必然没有分歧
+            parent = None
+            insert_point = self.root  # 置入 new_node 的位置
+            while True:
 
+                if isinstance(insert_point, bytes):
+                    break
+                elif isinstance(insert_point, CBInternal):
+                    if insert_point.diff_at > diff_at:
+                        break
+                    elif insert_point.diff_at == diff_at and insert_point.mask > node.mask:
+                        # 同一 byte, 分歧 bit 越靠后, 数值越小
+                        break
+
+                check_byte = new_bytes[insert_point.diff_at] if len(new_bytes) > insert_point.diff_at else 0
+                parent = insert_point
+                direct = (1 + (insert_point.mask | check_byte)) >> 8
+                if direct == 0:
+                    insert_point = insert_point.crit_is_0
+                else:
+                    insert_point = insert_point.crit_is_1
+
+            if parent is None:
+                self.root = node
+            else:
+                if direct == 0:
+                    parent.crit_is_0 = node
+                else:
+                    parent.crit_is_1 = node
+
+            if node.crit_is_0 is None:
+                node.crit_is_0 = insert_point
+            else:
+                node.crit_is_1 = insert_point
 
     def find_best_match(self, src: bytes):
         q = [None] * 3
